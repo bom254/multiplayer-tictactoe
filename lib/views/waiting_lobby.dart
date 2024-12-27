1793c230fa7b226ea5c669f6098039530b2a6772
bom254 <bevonmokuas@gmail.com>
2024-12-27 15:33:09 +0000

    added the waiting screen for player to join in

---
+++ lib/views/waiting_lobby.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class WaitingLobby extends StatefulWidget {
+  const WaitingLobby({super.key});
+
+  @override
+  State<WaitingLobby> createState() => _WaitingLobbyState();
+}
+
+class _WaitingLobbyState extends State<WaitingLobby> {
+  @override
+  void initState() {
+    super.initState();
+    roomIdController = TextEditingController(
+      text: Provider.of<RoomDataProvider>(context, listen: false).roomData['_id'],
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    roomIdController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Waiting for a player to join...'),
+        const SizedBox(height: 20),
+        CustomTextField(
+          controller: roomIdController,
+          hint: '',
+          isReadOnly: true,
+        ),
+      ],
+    );
+  }
+}
